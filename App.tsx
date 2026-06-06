a2c9d2a3da444c02443f6cddf5b1eb5bf99ce1ac
Vicienna <108596016+Vicienna@users.noreply.github.com>
2026-06-06 15:28:56 +0700

    Ngetest doang

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './hooks/useCart';
+import { Navbar } from './components/shop/Navbar';
+import LandingPage from './app/page';
+import ShopPage from './app/shop/page';
+import CheckoutPage from './app/shop/checkout/page';
+import AdminDashboard from './app/admin/page';
+import AdminLogin from './app/admin/login/page';
+import ProductManagement from './app/admin/products/page';
+import NotFound from './app/not-found';
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div className="min-h-screen bg-pink-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/shop/checkout" element={<CheckoutPage />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/products" element={<ProductManagement />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
